1deab8604904a2275a61a36dcd1a226bb717b4c1
Stelios Anastasakis <bpstelios10@hotmail.com>
2023-12-06 01:56:29 +0200

    [week5] Introduced files for week5 assignment, burrows.
    * Updated modules configs

---
+++ week5-burrows/src/CircularSuffixArray.java
@@ -0,0 +1,20 @@
+public class CircularSuffixArray {
+
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+    }
+
+    // length of s
+    public int length() {
+        return -1;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return -1;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+    }
+}
